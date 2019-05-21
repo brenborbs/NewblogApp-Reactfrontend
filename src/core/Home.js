04a2cd47d91336ef43db51e4ba3176b0556ef952
brenborbs <borbonbrennon@gmail.com>
2019-05-21 16:55:46 +0800

    Completed Signup/Signin Users front-end

---
+++ src/core/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+      <div className="jumbotron">
+          <h2>Home</h2>
+          <p className="lead">Welcome to React Frontend</p>
+      </div>
+      <div className="container">
+         Post component goes here...
+      </div>
+  </div>
+);
+
+export default Home;
